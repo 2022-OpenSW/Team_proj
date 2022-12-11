16c9d5f9dd1a21389ffc712cb1d4c1fb2cfc1a94
Jhyunee <jmmisung@gmail.com>
2022-12-12 04:07:25 +0900

    :bulb: Type error solved

--- default_runtime.py
+++ default_runtime.py
@@ -11,18 +11,18 @@ CLASSES = ('onepiece(dress)', 'skirt', 'pants', 'jumpsuite',
            'shirt', 'sweater', 'blouse', 'cardigan', 'jumper', 'jacket', 'coat' )
 
 mmdet.datasets.coco.CocoDataset.CLASSES = ('onepiece(dress)', 'skirt', 'pants', 'jumpsuite',
-                                           'shirt', 'sweater', 'blouse', 'cardigan', 'jumper', 'jacket', 'coat')
+                                           'shirt', 'sweater', 'blouse', 'cardigan', 'jumper', 'jacket', 'coat') 
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True, with_mask=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
 ]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
